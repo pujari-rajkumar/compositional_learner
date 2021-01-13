b1beb679cb43b35cc1468f199ddfdea91606fcb1
Rajkumar Pujari <rpujari@purdue.edu>
2021-01-12 23:01:20 -0500

    Adding learning tasks code

---
+++ learning_tasks/mentioned_entity_prediction_model.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# @author: Rajkumar Pujari
+
+
+import pickle
+import numpy as np
+from datetime import datetime
+import copy
+import random
+import math
+from sklearn.metrics import confusion_matrix
+import sys
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+from query_handling import data_splitting_utils
+from query_handling import graph_generation
+
+
+from compositional_learner.encoder import Encoder
+from compositional_learner.composer import Composer
+
+
+t1 = datetime.now()
+from learning_tasks.authorship_prediction_utils import batchify_data
+from learning_tasks.mentioned_entity_prediction_utils import load_data_batch
+t2 = datetime.now()
+print(t2-t1)
+
+
+fpath = './data/composite_learner_data/'
+
+
+class LinkPredictor(nn.Module):
+    
+    def __init__(self, args):
+        super(LinkPredictor, self).__init__()
+        self.args = args
+        self.encoder = Encoder(args)
+        self.composer = Composer(args)
+        self.ff1 = nn.Linear(5 * args.d_encoder, args.d_hidden)
+        self.nl1 = nn.Tanh()
+        self.do1 = nn.Dropout(p=args.dropout)
+        self.ff2 = nn.Linear(args.d_hidden, args.n_classes)
+        self.nl2 = nn.Tanh()
+        self.loss_fn = nn.CrossEntropyLoss()
+        params = [p for p in self.parameters() if p.requires_grad]
+        self.optimizer = optim.SGD(params, lr=args.lr, momentum=0.4, weight_decay=args.weight_decay)
+        if args.pretrained:
+            self.load_state_dict(torch.load(args.pretrained))
+            if args.init_linear:
+                nn.init.xavier_uniform_(self.ff1.weight)
+                nn.init.xavier_uniform_(self.ff2.weight)
+        
+    def forward(self, node_docs, node_mask, comb_amtx, src_ids, dest_ids):
+        batch_size = node_docs.size(0)
+        if self.args.cuda:
+            node_docs = node_docs.cuda()
+            node_mask = node_mask.cuda()
+            comb_amtx = comb_amtx.cuda()
+            src_ids = src_ids.cuda()
+            dest_ids = dest_ids.cuda()
+        graph_node_embs = self.encoder(node_docs, node_mask)
+        src_init_embs = graph_node_embs[torch.arange(batch_size), src_ids]
+        dest_init_embs = graph_node_embs[torch.arange(batch_size), dest_ids]
+        updated_node_embs, summary_emb = self.composer(graph_node_embs, comb_amtx)
+        src_embs = updated_node_embs[-1][torch.arange(batch_size), src_ids]
+        dest_embs  = updated_node_embs[-1][torch.arange(batch_size), dest_ids]
+        ff_input = torch.cat([src_init_embs, dest_init_embs, src_embs, dest_embs, summary_emb], dim=-1)
+        ff1_l = self.ff1(ff_input)
+        ff1_nl = self.nl1(ff1_l)
+        ff1_out = self.do1(ff1_nl)
+        ff2_l = self.ff2(ff1_out)
+        ff2_out = self.nl2(ff2_l)
+        out = ff2_out.view(batch_size, args.n_classes)
+        class_probs = F.softmax(out, dim=-1)
+        return class_probs
+    
+    def evaluate(self, test_data, log=sys.stdout):
+        self.eval()
+        test_batches = batchify_data(test_data[0], test_data[1], shuffle=False)
+        ncorr = 0
+        ntot = 0
+        y_true = []
+        y_pred = []
+        num_batch = 0
+        t1 = datetime.now()
+        with torch.no_grad():
+            for batch in test_batches:
+                node_docs, node_mask, comb_amtx, src_tensor, dest_tensor, y_tensor = batch
+                batch_out = self.forward(node_docs, node_mask, comb_amtx, src_tensor, dest_tensor)
+                batch_out = batch_out.cpu()
+                if torch.isnan(batch_out).any():
+                    print('NaN Error', file=log, flush=True)
+                y_pred += list(torch.argmax(batch_out, dim=1).data.numpy())
+                y_true += list(y_tensor.data.numpy())
+                ncorr += sum((torch.argmax(batch_out, dim=1).data == y_tensor.data).int()).item()
+                ntot += y_tensor.size(0)
+                num_batch += 1
+            print('Accuracy: ', round(ncorr * 100./ntot, 2), ncorr, ntot, file=log, flush=True)
+            y_true = np.array(y_true, dtype=int)
+            y_pred = np.array(y_pred, dtype=int)
+            print(confusion_matrix(y_true, y_pred), file=log, flush=True)
+            return ncorr * 100./ntot
+    
+    def train_model(self, train_data, val_data, num_epochs=3, save_path=fpath + 'saved_parameters/link_predictor.pt', log=sys.stdout):
+        best_val_perf = 0
+        t1 = datetime.now()
+        for epoch in range(num_epochs):
+            self.train()
+            train_batches = batchify_data(train_data[0], train_data[1], shuffle=True)
+            num_batch = 0
+            for batch in train_batches:
+                node_docs, node_mask, comb_amtx, src_tensor, dest_tensor, y_tensor = batch
+                batch_out = self.forward(node_docs, node_mask, comb_amtx, src_tensor, dest_tensor)
+                if self.args.cuda:
+                    y_tensor = y_tensor.cuda()
+                batch_loss = self.loss_fn(batch_out, y_tensor)
+                self.optimizer.zero_grad()
+                batch_loss.backward()
+                self.optimizer.step()
+                num_batch += 1
+                if num_batch % 1000 == 0:
+                    t2 = datetime.now()
+                    print(num_batch, ' done.', t2-t1, file=log, flush=True)
+            print('Epoch ', epoch, ' completed - ', t2 - t1, file=log, flush=True)
+            val_perf = self.evaluate(val_data, log=log)
+            if val_perf >= best_val_perf:
+                best_val_perf = val_perf
+                print('Saving parameters', file=log, flush=True)
+                torch.save(self.state_dict(), save_path)
+
+
+class Args:
+    def __init__(self):
+        self.cuda = True
+        self.d_encoder = 768
+        self.d_hidden = 384
+        self.n_heads = 12
+        self.d_k = 64
+        self.d_v = 64
+        self.n_layers = 2
+        self.dropout = 0.1
+        self.n_classes = 2
+        self.lr = 0.0075
+        self.weight_decay = 0
+        self.pretrained = None
+        self.init_linear = False
+
+
+args = Args()
+
+#Change this number if restarting after interruption after a certain batch with the next batch to be trained on
+already_done = 0
+
+
+t1 = datetime.now()
+query_batches = 100
+log = open(fpath + 'training_logs/log_r4056_t4056_phase2.txt', 'a+')
+if already_done > 0:
+    args.pretrained = fpath + 'saved_parameters/link_predictor_phase2_b' + str(already_done - 1) + '.pt'
+else:
+    state_dict = torch.load(fpath + 'saved_parameters/link_predictor_phase1_b99.pt')
+    encoder_state_dict = {}
+    for key in state_dict:
+        if key.startswith('encoder'):
+            encoder_state_dict[key[8:]] = state_dict[key].cpu()
+    composer_state_dict = {}
+    for key in state_dict:
+        if key.startswith('composer'):
+            composer_state_dict[key[9:]] = state_dict[key].cpu()
+torch.manual_seed(4056)
+link_predictor = LinkPredictor(args)
+if already_done == 0:
+    link_predictor.encoder.load_state_dict(encoder_state_dict)
+    link_predictor.composer.load_state_dict(composer_state_dict)
+    nn.init.xavier_uniform_(link_predictor.ff1.weight)
+    nn.init.xavier_uniform_(link_predictor.ff2.weight)
+for query_batch in range(already_done, query_batches):
+    random.seed(4056)
+    qdb_path = fpath + 'data_examples/query_dicts-' + str(query_batch) + '.pkl'
+    print('Loading data batch ', query_batch, file=log, flush=True)
+    qbeg = query_batch * 24
+    qend = (query_batch + 1) * 24
+    data = load_data_batch(qdb_path, log=log, trim_auth=True)
+    t2 = datetime.now()
+    print(t2 - t1, file=log, flush=True)
+    train, dev, test = data
+    print('Batch', query_batch, 'Train -', sum(train[1]) / len(train[1]), len(train[1]), file=log, flush=True)
+    print('Batch', query_batch, 'Dev -', sum(dev[1]) / len(dev[1]), len(dev[1]), file=log, flush=True)
+    print('Batch', query_batch, 'Test -', sum(test[1]) / len(test[1]), len(test[1]), file=log, flush=True)
+    with torch.cuda.device(1):
+        torch.manual_seed(4056)
+        link_predictor.cuda()
+        link_predictor.train_model(train, dev, log=log)
+        link_predictor.load_state_dict(torch.load(fpath + 'saved_parameters/link_predictor.pt'))
+        torch.save(link_predictor.state_dict(), fpath + 'saved_parameters/link_predictor_phase2_b' + str(query_batch) + '.pt')
+        print('Final Test Performance', file=log, flush=True)
+        link_predictor.evaluate(test, log=log)
+    t2 = datetime.now()
+    print('Batch', query_batch, 'done -', t2 - t1, file=log, flush=True)
+log.close()
+
---
+++ learning_tasks/mentioned_entity_prediction_utils.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pickle
+import numpy as np
+from datetime import datetime
+import copy
+import random
+import math
+from sklearn.metrics import confusion_matrix
+import sys
+import torch
+import json
+
+
+# In[2]:
+
+
+import utils
+from query_handling import data_splitting_utils
+from query_handling import graph_generation
+
+
+# In[3]:
+
+
+def get_doc_text(wparse):
+    ret_text = []
+    for para in wparse['tokens']:
+        for sent in para:
+            stext = ' '.join([tok['originalText'] for tok in sent['tokens']])
+            ret_text.append(stext)
+    return ret_text
+
+
+fpath = './data/composite_learner_data/'
+
+with open(fpath + 'formatted_tweet_parses.pkl', 'rb') as infile:
+    tweet_parses = pickle.load(infile)
+with open(fpath + 'tweet_doc_tensors_poprem.pkl', 'rb') as infile:
+    tweet_doc_tensors_poprem, tweet_popents = pickle.load(infile)
+tweet_texts = []
+for ent in tweet_parses:
+    for topic in tweet_parses[ent]:
+        for event in tweet_parses[ent][topic]:
+            for tparse in tweet_parses[ent][topic][event]:
+                tweet_texts.append((tparse['parse_id'], get_doc_text(tparse['parse'])))
+tweet_texts = sorted(tweet_texts, key=lambda x:x[0])
+tweet_texts = [t[1] for t in tweet_texts]
+print('Tweet data loaded.')
+    
+with open(fpath + 'stat_doc_tensors_poprem.pkl', 'rb') as infile:
+    stat_doc_tensors_poprem, stat_popents = pickle.load(infile)
+with open(fpath + 'stat_ent_mentions.pkl', 'rb') as infile:
+    stat_ppaths, _, _ = pickle.load(infile)
+stat_texts = [get_doc_text(json.load(open(stat_ppath))) for stat_ppath in stat_ppaths]
+print('Statement data loaded.')
+    
+with open(fpath + 'news_doc_tensors_poprem.pkl', 'rb') as infile:
+    news_doc_tensors_poprem, news_popents = pickle.load(infile)
+with open(fpath + 'news_ent_mentions.pkl', 'rb') as infile:
+    news_ppaths, _, _ = pickle.load(infile)
+news_texts = [get_doc_text(json.load(open(news_ppath))) for news_ppath in news_ppaths]
+print('News data loaded.')
+
+
+# In[7]:
+
+
+def is_same(r1_text, r2_text):
+    if len(r1_text) != len(r2_text):
+        return False
+    else:
+        for s1, s2 in zip(r1_text, r2_text):
+            if s1.lower().strip() != s2.lower().strip():
+                return False
+        return True
+    
+def get_index(doc_text, ref_texts):
+    for i, rt in enumerate(ref_texts):
+        if is_same(doc_text, rt):
+            return i
+    return -1
+
+def get_popular_entity(doc_ments):
+    doc_ents = {}
+    for sent in doc_ments:
+        for ment in sent:
+            if ment not in doc_ents:
+                doc_ents[ment] = 0
+            doc_ents[ment] += 1
+    if len(doc_ents) > 0:
+        doc_ent_list = [(ment, doc_ents[ment]) for ment in doc_ents]
+        sorted_de_list = sorted(doc_ent_list, key=lambda x:x[1], reverse=True)
+        return sorted_de_list[0][0]
+    else:
+        return None
+
+
+# In[8]:
+
+
+def make_examples(data_dict, data_cis, G, sel_cands, trim_auth=False, max_num=50):
+    data_x = []
+    data_y = []
+    G_n, G_i2n, G_a = G
+    num_ex = 0
+    
+    #For each selected candidate node
+    for cand in sel_cands:
+        if num_ex < max_num:
+            ct = utils.split_node_name(cand)
+            #Get the popular entity and popular entity-removed document embedding
+            if ct[1] == 'news':
+                _, dtype, topic, event, dd_idx = ct
+                event = int(event)
+                dd_idx = int(dd_idx)
+                cand_text = data_dict['news'][0][topic][event][dd_idx]
+                cand_idx = get_index(cand_text, news_texts)
+                cand_poprem = news_popents[cand_idx]
+                cand_rem_emb = news_doc_tensors_poprem[cand_idx]
+                
+            else:
+                _, dtype, a2, topic, event, dd_idx = ct
+                event = int(event)
+                dd_idx = int(dd_idx)
+                cand_text = data_dict[a2][dtype + 's'][0][topic][event][dd_idx]
+                if dtype == 'tweet':
+                    cand_idx = get_index(cand_text, tweet_texts)
+                    cand_poprem = tweet_popents[cand_idx]
+                    cand_rem_emb = tweet_doc_tensors_poprem[cand_idx]
+                else:
+                    cand_idx = get_index(cand_text, stat_texts)
+                    cand_poprem = stat_popents[cand_idx]
+                    cand_rem_emb = stat_doc_tensors_poprem[cand_idx]
+                
+            if cand_idx > 0 and cand_poprem:
+                mn_name = None
+                if cand_poprem in utils.graph_generation.wiki_ann_dict:
+                    mn_name = 'ref-' + utils.graph_generation.get_wiki_name(cand_poprem)
+                if mn_name in G_n and G_n[cand] in G_a[G_n[mn_name]]:
+                    #Remove the link between the popular referenced entity and document in the graph
+                    G_a[G_n[mn_name]].remove(G_n[cand])
+                    doc_tensors, node_dids, graph_amtx = utils.get_graph_tensors(data_dict, G)
+                    cand_did = node_dids[G_n[cand]][0]
+                    #Replace the document embedding with the popular entity-removed document embedding
+                    doc_tensors[cand_did] = np.squeeze(cand_rem_emb, axis=0)
+                    n1_ID = G_n[mn_name]
+                    n2_ID = G_n[cand]
+                    #Add the positive example
+                    eg, eg_size = utils.trim_example(G, doc_tensors, node_dids, graph_amtx, n1_ID, n2_ID, trim_auth=trim_auth)
+                    data_x.append(eg)
+                    data_y.append(1)
+                    #Find another referenced entity that was not mentioned in the original document for negative examples
+                    other_refs = [ID for ID in G_i2n.keys() if G_i2n[ID].startswith('ref-') and (n2_ID not in G_a[ID]) and ID != n1_ID]
+                    neg_ref = random.choice(other_refs)
+                    eg_doc_tensors, eg_node_dids, eg_graph_amtx, _, eg_n2_ID = eg
+                    neg_eg = (eg_doc_tensors, eg_node_dids, eg_graph_amtx, neg_ref, eg_n2_ID)
+                    data_x.append(neg_eg)
+                    data_y.append(0)
+                    #Add the link from entity to the popular entity back in the graph
+                    G_a[G_n[mn_name]].append(G_n[cand])
+                    num_ex += 1
+        else:
+            break
+    return data_x, data_y
+
+
+# In[9]:
+
+
+def get_examples(query_dict, qd_split, trim_auth=False, num_tr=25):
+    t, d, e = qd_split
+    tc = utils.count_docs(t)
+    tr_x = []
+    tr_y = []
+    de_x = []
+    de_y = []
+    te_x = []
+    te_y = []
+    
+    train_dict, train_cis = data_splitting_utils.get_documents(query_dict, [t])
+    train_graph = graph_generation.generate_graph(train_dict)
+    news_nodes, tweet_nodes, statement_nodes, quote_nodes, event_nodes, author_nodes =                                                            utils.get_node_lists(train_graph, train_cis, t)
+    cand_nodes = tweet_nodes + statement_nodes + news_nodes
+    
+    num_samples = min(2 * num_tr, len(cand_nodes))
+    sel_cands = random.sample(cand_nodes, num_samples)
+    train_x, train_y = make_examples(train_dict, train_cis, train_graph, sel_cands, trim_auth=trim_auth, max_num=num_tr)
+    
+    dev_dict, dev_cis = data_splitting_utils.get_documents(query_dict, [t, d])
+    dev_graph = graph_generation.generate_graph(dev_dict)
+    news_nodes, tweet_nodes, statement_nodes, quote_nodes, event_nodes, author_nodes =                                                            utils.get_node_lists(dev_graph, dev_cis, d)
+    cand_nodes = tweet_nodes + statement_nodes + news_nodes
+    num_samples = min(2 * num_tr, len(cand_nodes))
+    sel_cands = random.sample(cand_nodes, num_samples)
+    dev_x, dev_y = make_examples(dev_dict, dev_cis, dev_graph, sel_cands, trim_auth=trim_auth, max_num=0.2*num_tr)
+    
+    test_dict, test_cis = data_splitting_utils.get_documents(query_dict, [t, d, e])
+    test_graph = graph_generation.generate_graph(test_dict)
+    news_nodes, tweet_nodes, statement_nodes, quote_nodes, event_nodes, author_nodes =                                                            utils.get_node_lists(test_graph, test_cis, e)
+    cand_nodes = tweet_nodes + statement_nodes + news_nodes
+    num_samples = min(2 * num_tr, len(cand_nodes))
+    sel_cands = random.sample(cand_nodes, num_samples)
+    test_x, test_y = make_examples(test_dict, test_cis, test_graph, sel_cands, trim_auth=trim_auth, max_num=0.2*num_tr)
+    
+    
+    return (train_x, train_y), (dev_x, dev_y), (test_x, test_y)
+
+
+# In[11]:
+
+
+def batchify_sents(all_sents, batch_size=100):
+    batches = []
+    beg = 0
+    end = batch_size
+    while beg < len(all_sents):
+        batches.append(all_sents[beg:end])
+        beg = end
+        end += batch_size
+    return batches
+
+
+# In[12]:
+
+
+def compute_embeddings(all_docs, batch_size=50, log=sys.stdout):
+    ret_embs = []
+    doc_lens = [len(doc) for doc in all_docs]
+    all_sents = []
+    for doc in all_docs:
+        all_sents += doc
+    batches = batchify_sents(all_sents, batch_size=batch_size) 
+    
+    print(len(batches), ' batches created.', file=log, flush=True)
+    
+    tot = 0
+    bnum = 0
+    t1 = datetime.now()
+    all_sents_tensor = torch.FloatTensor(0, 768)
+    
+    for batch in batches:
+        try:
+            batch_tensor = create_bert_emb(batch)
+        except:
+            batch_tensor = torch.FloatTensor(len(batch), 768)
+        all_sents_tensor = torch.cat([all_sents_tensor, batch_tensor], dim=0)
+        bnum += 1
+        tot += len(batch)
+        if bnum % 500 == 0:
+            t2 = datetime.now()
+            print(tot, '/', len(all_sents), ' done - ', t2 - t1, file=log, flush=True)
+    
+    b = 0
+    for dlen in doc_lens:
+        e = b + dlen
+        ret_embs.append(all_sents_tensor[b:e, :].data.numpy())
+        b = e
+
+    return ret_embs
+
+
+# In[10]:
+
+
+def load_data_batch(qdb_path, log=sys.stdout, trim_auth=False):
+    with open(qdb_path, 'rb') as infile:
+        query_dicts, qd_splits = pickle.load(infile)
+    i = 0
+    train_x = []
+    train_y = []
+    dev_x = []
+    dev_y = []
+    test_x = []
+    test_y = []
+    t1 = datetime.now()
+    for query_dict, qd_split in zip(query_dicts, qd_splits):
+        t, d, e = get_examples(query_dict, qd_split, trim_auth=True)
+        train_x += t[0]
+        train_y += t[1]
+        dev_x += d[0]
+        dev_y += d[1]
+        test_x += e[0]
+        test_y += e[1]
+        i += 1
+        t2 = datetime.now()
+        print(i, 'done', t2-t1, file=log, flush=True)
+    
+    print(len(train_x), len(dev_x), len(test_x), file=log, flush=True)
+   
+    data = ((train_x, train_y), (dev_x, dev_y), (test_x, test_y))
+    return data
+
